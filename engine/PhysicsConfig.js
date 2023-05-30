a6cf5ede7fa4c2f3b95aa164f28a501ae6654b79
sloont <colinabel93@gmail.com>
2023-05-30 14:58:18 -0400

    formatting

--- engine/PhysicsConfig.js
+++ engine/PhysicsConfig.js
@@ -1,101 +1,101 @@
-var taroPhysicsConfig = {
-	/*
-	 * NOT IN USE
-	*/
+// var taroPhysicsConfig = {
+// 	/*
+// 	 * NOT IN USE
+// 	*/
 
-	taroPhysicsChoices: {
-		/* Includes for the main taro loader. Flags are indicated as:
-		* c = client
-		* s = server
-		* a =
-		* p = prototype
-		*/
+// 	taroPhysicsChoices: {
+// 		/* Includes for the main taro loader. Flags are indicated as:
+// 		* c = client
+// 		* s = server
+// 		* a =
+// 		* p = prototype
+// 		*/
 
-		/* Physics Libraries */
-		planck: [
-			['csap', 'PhysicsComponent', './components/physics/box2d/Box2dComponent.js'],
-			['csap', 'TaroEntityPhysics', './components/physics/box2d/TaroEntityPhysics.js'],
-			['csap', 'TaroBox2dWorld', './components/physics/box2d/TaroBox2dDebugPainter.js'],
-			['csap', 'dists', './components/physics/box2d/dists.js'],
-			['csap', 'planck', './components/physics/box2d/dists/planck/planck.js']
-		],
+// 		/* Physics Libraries */
+// 		planck: [
+// 			['csap', 'PhysicsComponent', './components/physics/box2d/Box2dComponent.js'],
+// 			['csap', 'TaroEntityPhysics', './components/physics/box2d/TaroEntityPhysics.js'],
+// 			['csap', 'TaroBox2dWorld', './components/physics/box2d/TaroBox2dDebugPainter.js'],
+// 			['csap', 'dists', './components/physics/box2d/dists.js'],
+// 			['csap', 'planck', './components/physics/box2d/dists/planck/planck.js']
+// 		],
 
-		box2dweb: [
-			['csap', 'PhysicsComponent', './components/physics/box2d/Box2dComponent.js'],
-			['csap', 'TaroEntityPhysics', './components/physics/box2d/TaroEntityPhysics.js'],
-			['csap', 'TaroBox2dWorld', './components/physics/box2d/TaroBox2dDebugPainter.js'],
-			['csap', 'dists', './components/physics/box2d/dists.js'],
-			['csap', 'box2dweb', './components/physics/box2d/dists/box2dweb/lib_box2d.js', 'box2dweb'],
-			['csap', 'box2dninja', './components/physics/box2d/dists/box2dweb/box2d_ninja.js', 'box2dninja']
-		],
+// 		box2dweb: [
+// 			['csap', 'PhysicsComponent', './components/physics/box2d/Box2dComponent.js'],
+// 			['csap', 'TaroEntityPhysics', './components/physics/box2d/TaroEntityPhysics.js'],
+// 			['csap', 'TaroBox2dWorld', './components/physics/box2d/TaroBox2dDebugPainter.js'],
+// 			['csap', 'dists', './components/physics/box2d/dists.js'],
+// 			['csap', 'box2dweb', './components/physics/box2d/dists/box2dweb/lib_box2d.js', 'box2dweb'],
+// 			['csap', 'box2dninja', './components/physics/box2d/dists/box2dweb/box2d_ninja.js', 'box2dninja']
+// 		],
 
-		box2dts: [
-			['csap', 'PhysicsComponent', './components/physics/box2d/Box2dComponent.js'],
-			['csap', 'TaroEntityPhysics', './components/physics/box2d/TaroEntityPhysics.js'],
-			['csap', 'TaroBox2dWorld', './components/physics/box2d/TaroBox2dDebugPainter.js'],
-			['csap', 'dists', './components/physics/box2d/dists.js'],
-			['csap', 'box2dts', './components/physics/box2d/dists/flyoverbox2dts/bundle.js']
-		],
+// 		box2dts: [
+// 			['csap', 'PhysicsComponent', './components/physics/box2d/Box2dComponent.js'],
+// 			['csap', 'TaroEntityPhysics', './components/physics/box2d/TaroEntityPhysics.js'],
+// 			['csap', 'TaroBox2dWorld', './components/physics/box2d/TaroBox2dDebugPainter.js'],
+// 			['csap', 'dists', './components/physics/box2d/dists.js'],
+// 			['csap', 'box2dts', './components/physics/box2d/dists/flyoverbox2dts/bundle.js']
+// 		],
 
-		crash: [
-			['csap', 'PhysicsComponent', './components/physics/crash/CrashComponent.js'],
-			['csap', 'TaroEntityPhysics', './components/physics/crash/TaroEntityPhysics.js'],
-			['csap', 'CollisionController', './components/physics/crash/CollisionController.js'],
-			['csap', 'quickselect', './components/physics/crash/crashDependencies/quickselect.js'],
-			['csap', 'rbush', './components/physics/crash/crashDependencies/rbush.js'],
-			['csap', 'sat', './components/physics/crash/crashDependencies/sat.js'],
-			['csap', 'crash', './components/physics/crash/crashDependencies/crash.js', 'crash']
-		]
-	},
+// 		crash: [
+// 			['csap', 'PhysicsComponent', './components/physics/crash/CrashComponent.js'],
+// 			['csap', 'TaroEntityPhysics', './components/physics/crash/TaroEntityPhysics.js'],
+// 			['csap', 'CollisionController', './components/physics/crash/CollisionController.js'],
+// 			['csap', 'quickselect', './components/physics/crash/crashDependencies/quickselect.js'],
+// 			['csap', 'rbush', './components/physics/crash/crashDependencies/rbush.js'],
+// 			['csap', 'sat', './components/physics/crash/crashDependencies/sat.js'],
+// 			['csap', 'crash', './components/physics/crash/crashDependencies/crash.js', 'crash']
+// 		]
+// 	},
 
-	gameClasses: [
-		['csap', 'Unit', '../src/gameClasses/Unit.js'],
-		['csap', 'MapComponent', '../src/gameClasses/components/MapComponent.js'],
-		['csap', 'Region', '../src/gameClasses/Region.js'],
-		['csap', 'Item', '../src/gameClasses/Item.js'],
-		['csap', 'Projectile', '../src/gameClasses/Projectile.js'],
-		['csap', 'Particle', '../src/gameClasses/Particle.js'],
-		['csap', 'Sensor', '../src/gameClasses/Sensor.js']
-	],
+// 	gameClasses: [
+// 		['csap', 'Unit', '../src/gameClasses/Unit.js'],
+// 		['csap', 'MapComponent', '../src/gameClasses/components/MapComponent.js'],
+// 		['csap', 'Region', '../src/gameClasses/Region.js'],
+// 		['csap', 'Item', '../src/gameClasses/Item.js'],
+// 		['csap', 'Projectile', '../src/gameClasses/Projectile.js'],
+// 		['csap', 'Particle', '../src/gameClasses/Particle.js'],
+// 		['csap', 'Sensor', '../src/gameClasses/Sensor.js']
+// 	],
 
-	loadSelectPhysics: function (physicsChoice) {
-		console.log('loadSelectPhysics', physicsChoice)
-		this.loadFiles(this.taroPhysicsChoices[physicsChoice]);
-	},
+// 	loadSelectPhysics: function (physicsChoice) {
+// 		console.log('loadSelectPhysics', physicsChoice)
+// 		this.loadFiles(this.taroPhysicsChoices[physicsChoice]);
+// 	},
 
-	loadPhysicsGameClasses: function () {
-		console.log('loadPhysicsGameClasses', this.gameClasses)
-		this.loadFiles(this.gameClasses);
-	},
+// 	loadPhysicsGameClasses: function () {
+// 		console.log('loadPhysicsGameClasses', this.gameClasses)
+// 		this.loadFiles(this.gameClasses);
+// 	},
 
-	// I don't think the server needs to be loading crash dependencies if we have them as npm packages.
-	// Should test removing the 's' from those item[0] strings.
+// 	// I don't think the server needs to be loading crash dependencies if we have them as npm packages.
+// 	// Should test removing the 's' from those item[0] strings.
 
-	loadFiles: function (physicsFiles) {
-		var arr = physicsFiles;
-		var arrCount = arr.length;
-		var arrIndex, arrItem, itemJs;
+// 	loadFiles: function (physicsFiles) {
+// 		var arr = physicsFiles;
+// 		var arrCount = arr.length;
+// 		var arrIndex, arrItem, itemJs;
 
-		var argParse = require('node-arguments').process;
-		var args = argParse(process.argv, { separator: '-' });
+// 		var argParse = require('node-arguments').process;
+// 		var args = argParse(process.argv, { separator: '-' });
 
-		if (!args['-deploy']) {
-			// Loop the taroCoreConfig object's include array
-			// and load the required files
-			for (arrIndex = 0; arrIndex < arrCount; arrIndex++) {
-				arrItem = arr[arrIndex];
+// 		if (!args['-deploy']) {
+// 			// Loop the taroCoreConfig object's include array
+// 			// and load the required files
+// 			for (arrIndex = 0; arrIndex < arrCount; arrIndex++) {
+// 				arrItem = arr[arrIndex];
 
-				itemJs = `${arrItem[1]} = ` + `require("${arrItem[2]}")`;
-				// Check if there is a specific object we want to use from the
-				// module we are loading
-				if (arrItem[3]) {
-					itemJs += `.${arrItem[3]};`;
-				} else {
-					itemJs += ';';
-				}
-				eval(itemJs);
-			}
-		}
-	}
+// 				itemJs = `${arrItem[1]} = ` + `require("${arrItem[2]}")`;
+// 				// Check if there is a specific object we want to use from the
+// 				// module we are loading
+// 				if (arrItem[3]) {
+// 					itemJs += `.${arrItem[3]};`;
+// 				} else {
+// 					itemJs += ';';
+// 				}
+// 				eval(itemJs);
+// 			}
+// 		}
+// 	}
 };
 if (typeof (module) !== 'undefined' && typeof (module.exports) !== 'undefined') { module.exports = taroPhysicsConfig; }
