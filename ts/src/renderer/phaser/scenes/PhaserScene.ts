213306eb35bda947ca6d09dfbd5255f7b1a0b517
Pankaj Patidar <pankaj9296@gmail.com>
2023-09-20 18:28:47 +0530

    cache update: proxy all s3 requests via cloudflare

--- ts/src/renderer/phaser/scenes/PhaserScene.ts
+++ ts/src/renderer/phaser/scenes/PhaserScene.ts
@@ -1,6 +1,12 @@
 class PhaserScene extends Phaser.Scene {
 
 	patchAssetUrl (url: string): string {
+		
+		// proxy all s3 requests via cloudflare
+		if (url.startsWith('https://modd.s3.amazonaws.com')) {
+			url = url.replace('https://modd.s3.amazonaws.com', 'https://old-cache.modd.io');
+		}
+		
 		// https://stackoverflow.com/a/37455118
 		return `${url}?v=1`;
 	}
