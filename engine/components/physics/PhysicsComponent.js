46f29757e457a8b8cf9bb950e4ad3502afb17840
Nick van Urk <nickyvanurk@gmx.com>
2024-05-13 23:44:20 +0200

    Init rapier

--- engine/components/physics/PhysicsComponent.js
+++ engine/components/physics/PhysicsComponent.js
@@ -12,9 +12,6 @@ class PhysicsComponent extends TaroEventingClass {
         super();
         this.classId = 'PhysicsComponent';
         this.componentId = 'physics';
-        // experimental
-        this._scaleRatio = 30;
-        this.avgPhysicsTickDuration = 20.0;
         this.engine = options.engine;
         this._entity = entity;
         this._options = options;
@@ -25,131 +22,35 @@ class PhysicsComponent extends TaroEventingClass {
     }
     load() {
         return __awaiter(this, void 0, void 0, function* () {
-            this.simulation =
-                this.engine === 'rapier' ? new RapierComponent() : new Box2dComponent(this._entity, this._options);
+            console.log('..........................................');
+            this.simulation = new Box2dComponent(this._entity, this._options);
             yield this.simulation.load();
             this._callback();
+            yield RAPIER.init();
+            let gravity = { x: 0.0, y: -9.81, z: 0.0 };
+            let world = new RAPIER.World(gravity);
+            this.world = world;
+            // Create the ground
+            let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 0.1, 10.0);
+            world.createCollider(groundColliderDesc);
+            // Create a dynamic rigid-body.
+            let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic().setTranslation(0.0, 1.0, 0.0);
+            let rigidBody = world.createRigidBody(rigidBodyDesc);
+            // Create a cuboid collider attached to the dynamic rigidBody.
+            let colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5);
+            let collider = world.createCollider(colliderDesc, rigidBody);
+            console.log(world);
+            // Game loop. Replace by your own game loop system.
+            let gameLoop = () => {
+                // Ste the simulation forward.
+                world.step();
+                // Get and print the rigid-body's position.
+                let position = rigidBody.translation();
+                console.log('Rigid-body position: ', position.x, position.y, position.z);
+                setTimeout(gameLoop, 16);
+            };
+            gameLoop();
         });
-    }
-    /* CONVERT TO COMMENT BLOCKS */
-    createWorld() {
-        //@ts-ignore
-        this.simulation.createWorld();
-    }
-    gravity(x, y) {
-        //@ts-ignore
-        this.simulation.gravity(x, y);
-    }
-    setContinuousPhysics(continuousPhysics) {
-        //@ts-ignore
-        this.simulation.setContinuousPhysics(continuousPhysics);
-    }
-    // b2d unused
-    useWorker() { }
-    // b2d get/set
-    mode(val) {
-        return !isNaN(val) ? this.simulation.mode(val) : this.simulation.mode();
-    }
-    // b2d get/set
-    sleep(val) {
-        return !isNaN(val) ? this.simulation.sleep(val) : this.simulation.sleep();
-    }
-    // b2d get/set
-    scaleRatio(val) {
-        return !isNaN(val) ? this.simulation.scaleRatio(val) : this.simulation.scaleRatio();
-    }
-    // b2d get/set
-    tilesizeRatio(val) {
-        return !isNaN(val) ? this.simulation.tilesizeRatio(val) : this.simulation.tilesizeRatio();
-    }
-    // b2d get
-    world() {
-        return this.simulation.world();
-    }
-    // b2d create fixture from params and fixture def
-    createFixture(params) {
-        return this.simulation.createFixture(params);
-    }
-    // b2d create body from definition and assign it to provided entity
-    createBody(entity, body, isLossTolerant) {
-        this.simulation.createBody(entity, body, isLossTolerant || false);
-    }
-    // b2d destroy body and remove it from entity
-    destroyBody(entity, body) {
-        this.simulation.destroyBody(entity, body);
-    }
-    // b2d unused
-    createJoint() { }
-    // b2d unused
-    destroyJoint() { }
-    // b2d check rectangle for entities
-    getBodiesInRegion(region) {
-        return this.simulation.getBodiesInRegion(region);
-    }
-    // b2d create physics bodies for map walls
-    staticsFromMap(mapLayer, callback) {
-        this.simulation.staticsFromMap(mapLayer, callback);
-    }
-    // b2d destroy all walls
-    destroyWalls() {
-        this.simulation.destroyWalls();
-    }
-    // b2d assign callbacks for different parts of a contact's lifetime
-    contactListener(beginContact, endContact, preSolve, postSolve) {
-        this.simulation.contactListener(beginContact, endContact, preSolve, postSolve);
-    }
-    // b2d get/set - disable collision generation for the world
-    networkDebugMode(val) {
-        return val == undefined ? this.simulation.networkDebugMode() : this.simulation.networkDebugMode(val);
-    }
-    // b2d (except planck) enable physics debug draw graphics
-    enableDebug(mountScene) {
-        this.simulation.enableDebug(mountScene);
-    }
-    // b2d get/set callback for every physics world step
-    updateCallback(callback) {
-        return callback ? this.simulation.updateCallback(callback) : this.simulation.updateCallback();
-    }
-    // b2d
-    start() {
-        this.simulation.start();
-    }
-    // b2d
-    stop() {
-        this.simulation.stop();
-    }
-    // b2d add physics actions to the engine's queue
-    queueAction(action) {
-        this.simulation.queueAction(action);
-    }
-    // b2d core simulation update method
-    // stepped by TaroEngine
-    update(dt) {
-        this.simulation.update(dt);
-    }
-    // b2d destroy physics simulation
-    destroy() {
-        this.simulation.destroy();
-    }
-    // b2d call engine script triggers on contact begin
-    _triggerContactEvent(entityA, entityB) {
-        this.simulation._triggerContactEvent(entityA, entityB);
-    }
-    // b2d call engine script triggers on contact end
-    _triggerLeaveEvent(entityA, entityB) {
-        this.simulation._triggerLeaveEvent(entityA, entityB);
-    }
-    // b2d taro callback for beginning of a contact
-    _beginContactCallback(contact) {
-        this.simulation._beginContactCallback(contact);
-    }
-    // b2d taro callback for end of a contact
-    _endContactCallback(contact) {
-        this.simulation._endContactCallback(contact);
-    }
-    // b2d adds taro contact callbacks via contactListener
-    _enableContactListener() {
-        this.simulation._enableContactListener();
     }
 }
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
--- ts/engine/components/physics/PhysicsComponent.ts
+++ ts/engine/components/physics/PhysicsComponent.ts
@@ -9,9 +9,10 @@ class PhysicsComponent extends TaroEventingClass {
 	engine: string;
 	simulation: Box2dComponent | RapierComponent;
 	// experimental
-	_scaleRatio = 30;
+	// _scaleRatio = 30;
+	// avgPhysicsTickDuration = 20.0;
 
-	avgPhysicsTickDuration = 20.0;
+	world: RAPIER.World;
 
 	constructor(entity: TaroEngine, options: PhysicsOptions, callback: () => void) {
 		super();
@@ -26,136 +27,169 @@ class PhysicsComponent extends TaroEventingClass {
 	}
 
 	async load(): Promise<void> {
-		this.simulation =
-			this.engine === 'rapier' ? new RapierComponent() : new Box2dComponent(this._entity, this._options);
+		this.simulation = new Box2dComponent(this._entity, this._options);
 		await this.simulation.load();
 		this._callback();
+
+		await RAPIER.init();
+
+		let gravity = { x: 0.0, y: -9.81, z: 0.0 };
+		let world = new RAPIER.World(gravity);
+		this.world = world;
+
+		// Create the ground
+		let groundColliderDesc = RAPIER.ColliderDesc.cuboid(10.0, 0.1, 10.0);
+		world.createCollider(groundColliderDesc);
+
+		// Create a dynamic rigid-body.
+		let rigidBodyDesc = RAPIER.RigidBodyDesc.dynamic().setTranslation(0.0, 1.0, 0.0);
+		let rigidBody = world.createRigidBody(rigidBodyDesc);
+
+		// Create a cuboid collider attached to the dynamic rigidBody.
+		let colliderDesc = RAPIER.ColliderDesc.cuboid(0.5, 0.5, 0.5);
+		let collider = world.createCollider(colliderDesc, rigidBody);
+
+		console.log(world);
+
+		// Game loop. Replace by your own game loop system.
+		let gameLoop = () => {
+			// Ste the simulation forward.
+			world.step();
+
+			// Get and print the rigid-body's position.
+			let position = rigidBody.translation();
+			console.log('Rigid-body position: ', position.x, position.y, position.z);
+
+			setTimeout(gameLoop, 16);
+		};
+
+		gameLoop();
 	}
 
-	/* CONVERT TO COMMENT BLOCKS */
+	// /* CONVERT TO COMMENT BLOCKS */
 
-	createWorld(): void {
-		//@ts-ignore
-		this.simulation.createWorld();
-	}
+	// createWorld(): void {
+	// 	//@ts-ignore
+	// 	this.simulation.createWorld();
+	// }
 
-	gravity(x: number, y: number): void {
-		//@ts-ignore
-		this.simulation.gravity(x, y);
-	}
+	// gravity(x: number, y: number): void {
+	// 	//@ts-ignore
+	// 	this.simulation.gravity(x, y);
+	// }
 
-	setContinuousPhysics(continuousPhysics: boolean) {
-		//@ts-ignore
-		this.simulation.setContinuousPhysics(continuousPhysics);
-	}
+	// setContinuousPhysics(continuousPhysics: boolean) {
+	// 	//@ts-ignore
+	// 	this.simulation.setContinuousPhysics(continuousPhysics);
+	// }
 
-	// b2d unused
-	useWorker(): void {}
-	// b2d get/set
-	mode(val?: number): TaroEngine | number {
-		return !isNaN(val) ? this.simulation.mode(val) : this.simulation.mode();
-	}
-	// b2d get/set
-	sleep(val?: number): TaroEngine | number {
-		return !isNaN(val) ? this.simulation.sleep(val) : this.simulation.sleep();
-	}
-	// b2d get/set
-	scaleRatio(val?: number): TaroEngine | number {
-		return !isNaN(val) ? this.simulation.scaleRatio(val) : this.simulation.scaleRatio();
-	}
-	// b2d get/set
-	tilesizeRatio(val?: number): TaroEngine | number {
-		return !isNaN(val) ? this.simulation.tilesizeRatio(val) : this.simulation.tilesizeRatio();
-	}
-	// b2d get
-	world(): b2World {
-		return this.simulation.world();
-	}
-	// b2d create fixture from params and fixture def
-	createFixture(params: Record<string, any>): Record<string, any> {
-		return this.simulation.createFixture(params);
-	}
-	// b2d create body from definition and assign it to provided entity
-	createBody(entity: TaroEntity, body: b2Body, isLossTolerant?: boolean): void {
-		this.simulation.createBody(entity, body, isLossTolerant || false);
-	}
-	// b2d destroy body and remove it from entity
-	destroyBody(entity: TaroEntity, body: b2Body): void {
-		this.simulation.destroyBody(entity, body);
-	}
-	// b2d unused
-	createJoint(): void {}
-	// b2d unused
-	destroyJoint(): void {}
-	// b2d check rectangle for entities
-	getBodiesInRegion(region: TaroEntity & { width: number; height: number; x: number; y: number }): TaroEntity[] {
-		return this.simulation.getBodiesInRegion(region);
-	}
-	// b2d create physics bodies for map walls
-	staticsFromMap(mapLayer: any, callback: () => any): void {
-		this.simulation.staticsFromMap(mapLayer, callback);
-	}
-	// b2d destroy all walls
-	destroyWalls(): void {
-		this.simulation.destroyWalls();
-	}
-	// b2d assign callbacks for different parts of a contact's lifetime
-	contactListener(beginContact?: () => any, endContact?: () => any, preSolve?: () => any, postSolve?: () => any): void {
-		this.simulation.contactListener(beginContact, endContact, preSolve, postSolve);
-	}
-	// b2d get/set - disable collision generation for the world
-	networkDebugMode(val?: boolean): TaroEngine | boolean {
-		return val == undefined ? this.simulation.networkDebugMode() : this.simulation.networkDebugMode(val);
-	}
-	// b2d (except planck) enable physics debug draw graphics
-	enableDebug(mountScene?: TaroEntity): void {
-		this.simulation.enableDebug(mountScene);
-	}
-	// b2d get/set callback for every physics world step
-	updateCallback(callback?: () => any): TaroEngine | (() => any) {
-		return callback ? this.simulation.updateCallback(callback) : this.simulation.updateCallback();
-	}
-	// b2d
-	start(): void {
-		this.simulation.start();
-	}
-	// b2d
-	stop(): void {
-		this.simulation.stop();
-	}
-	// b2d add physics actions to the engine's queue
-	queueAction(action: { type: string } & Record<string, any>): void {
-		this.simulation.queueAction(action);
-	}
-	// b2d core simulation update method
-	// stepped by TaroEngine
-	update(dt: number): void {
-		this.simulation.update(dt);
-	}
-	// b2d destroy physics simulation
-	destroy(): void {
-		this.simulation.destroy();
-	}
-	// b2d call engine script triggers on contact begin
-	_triggerContactEvent(entityA: TaroEntity, entityB: TaroEntity): void {
-		this.simulation._triggerContactEvent(entityA, entityB);
-	}
-	// b2d call engine script triggers on contact end
-	_triggerLeaveEvent(entityA: TaroEntity, entityB: TaroEntity): void {
-		this.simulation._triggerLeaveEvent(entityA, entityB);
-	}
-	// b2d taro callback for beginning of a contact
-	_beginContactCallback(contact: any): void {
-		this.simulation._beginContactCallback(contact);
-	}
-	// b2d taro callback for end of a contact
-	_endContactCallback(contact: any): void {
-		this.simulation._endContactCallback(contact);
-	}
-	// b2d adds taro contact callbacks via contactListener
-	_enableContactListener(): void {
-		this.simulation._enableContactListener();
-	}
+	// // b2d unused
+	// useWorker(): void {}
+	// // b2d get/set
+	// mode(val?: number): TaroEngine | number {
+	// 	return !isNaN(val) ? this.simulation.mode(val) : this.simulation.mode();
+	// }
+	// // b2d get/set
+	// sleep(val?: number): TaroEngine | number {
+	// 	return !isNaN(val) ? this.simulation.sleep(val) : this.simulation.sleep();
+	// }
+	// // b2d get/set
+	// scaleRatio(val?: number): TaroEngine | number {
+	// 	return !isNaN(val) ? this.simulation.scaleRatio(val) : this.simulation.scaleRatio();
+	// }
+	// // b2d get/set
+	// tilesizeRatio(val?: number): TaroEngine | number {
+	// 	return !isNaN(val) ? this.simulation.tilesizeRatio(val) : this.simulation.tilesizeRatio();
+	// }
+	// // b2d get
+	// world(): b2World {
+	// 	return this.simulation.world();
+	// }
+	// // b2d create fixture from params and fixture def
+	// createFixture(params: Record<string, any>): Record<string, any> {
+	// 	return this.simulation.createFixture(params);
+	// }
+	// // b2d create body from definition and assign it to provided entity
+	// createBody(entity: TaroEntity, body: b2Body, isLossTolerant?: boolean): void {
+	// 	this.simulation.createBody(entity, body, isLossTolerant || false);
+	// }
+	// // b2d destroy body and remove it from entity
+	// destroyBody(entity: TaroEntity, body: b2Body): void {
+	// 	this.simulation.destroyBody(entity, body);
+	// }
+	// // b2d unused
+	// createJoint(): void {}
+	// // b2d unused
+	// destroyJoint(): void {}
+	// // b2d check rectangle for entities
+	// getBodiesInRegion(region: TaroEntity & { width: number; height: number; x: number; y: number }): TaroEntity[] {
+	// 	return this.simulation.getBodiesInRegion(region);
+	// }
+	// // b2d create physics bodies for map walls
+	// staticsFromMap(mapLayer: any, callback: () => any): void {
+	// 	this.simulation.staticsFromMap(mapLayer, callback);
+	// }
+	// // b2d destroy all walls
+	// destroyWalls(): void {
+	// 	this.simulation.destroyWalls();
+	// }
+	// // b2d assign callbacks for different parts of a contact's lifetime
+	// contactListener(beginContact?: () => any, endContact?: () => any, preSolve?: () => any, postSolve?: () => any): void {
+	// 	this.simulation.contactListener(beginContact, endContact, preSolve, postSolve);
+	// }
+	// // b2d get/set - disable collision generation for the world
+	// networkDebugMode(val?: boolean): TaroEngine | boolean {
+	// 	return val == undefined ? this.simulation.networkDebugMode() : this.simulation.networkDebugMode(val);
+	// }
+	// // b2d (except planck) enable physics debug draw graphics
+	// enableDebug(mountScene?: TaroEntity): void {
+	// 	this.simulation.enableDebug(mountScene);
+	// }
+	// // b2d get/set callback for every physics world step
+	// updateCallback(callback?: () => any): TaroEngine | (() => any) {
+	// 	return callback ? this.simulation.updateCallback(callback) : this.simulation.updateCallback();
+	// }
+	// // b2d
+	// start(): void {
+	// 	this.simulation.start();
+	// }
+	// // b2d
+	// stop(): void {
+	// 	this.simulation.stop();
+	// }
+	// // b2d add physics actions to the engine's queue
+	// queueAction(action: { type: string } & Record<string, any>): void {
+	// 	this.simulation.queueAction(action);
+	// }
+	// // b2d core simulation update method
+	// // stepped by TaroEngine
+	// update(dt: number): void {
+	// 	this.simulation.update(dt);
+	// }
+	// // b2d destroy physics simulation
+	// destroy(): void {
+	// 	this.simulation.destroy();
+	// }
+	// // b2d call engine script triggers on contact begin
+	// _triggerContactEvent(entityA: TaroEntity, entityB: TaroEntity): void {
+	// 	this.simulation._triggerContactEvent(entityA, entityB);
+	// }
+	// // b2d call engine script triggers on contact end
+	// _triggerLeaveEvent(entityA: TaroEntity, entityB: TaroEntity): void {
+	// 	this.simulation._triggerLeaveEvent(entityA, entityB);
+	// }
+	// // b2d taro callback for beginning of a contact
+	// _beginContactCallback(contact: any): void {
+	// 	this.simulation._beginContactCallback(contact);
+	// }
+	// // b2d taro callback for end of a contact
+	// _endContactCallback(contact: any): void {
+	// 	this.simulation._endContactCallback(contact);
+	// }
+	// // b2d adds taro contact callbacks via contactListener
+	// _enableContactListener(): void {
+	// 	this.simulation._enableContactListener();
+	// }
 }
 
 type PhysicsOptions = {
