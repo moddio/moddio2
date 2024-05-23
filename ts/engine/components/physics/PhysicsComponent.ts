a0572993e207d90bdfcad64e2c17a7c5d9791392
Nick van Urk <nickyvanurk@gmx.com>
2024-05-23 18:58:39 +0200

    Generate collision shapes for the world

--- ts/engine/components/physics/PhysicsComponent.ts
+++ ts/engine/components/physics/PhysicsComponent.ts
@@ -41,7 +41,71 @@ class PhysicsComponent extends TaroEventingClass {
 		this.world = world;
 
 		let groundColliderDesc = RAPIER.ColliderDesc.cuboid(1000.0, 0.1, 1000.0);
+		groundColliderDesc.setTranslation(0.0, -0.5, 0.0);
 		world.createCollider(groundColliderDesc);
+
+		const floorLayer = taro.game.data.map.layers.find((l) => l.name === 'floor');
+		const wallsLayer = taro.game.data.map.layers.find((l) => l.name === 'walls');
+		const treesLayer = taro.game.data.map.layers.find((l) => l.name === 'trees');
+		console.log(floorLayer);
+
+		for (let y = 0; y < floorLayer.height; y++) {
+			const row = [];
+			for (let x = 0; x < floorLayer.width; x++) {
+				row.push(floorLayer.data[y * floorLayer.width + x]);
+			}
+			console.log(...row);
+		}
+
+		// const w = floorLayer.width;
+		// const h = floorLayer.height;
+		// let floorDesc = RAPIER.ColliderDesc.cuboid(w * 0.5, 0.5, h * 0.5);
+		// floorDesc.setTranslation(w * 0.5, 0, h * 0.5);
+		// world.createCollider(floorDesc);
+
+		// let numTileLayers = 0;
+		// for (const [idx, layer] of config.entries()) {
+		// 	if (layer.type === 'tilelayer' && layer.data) {
+		// 		const voxelsData = Voxels.generateVoxelsFromLayerData(layer, numTileLayers, false);
+		// 		voxels.updateLayer(voxelsData, idx);
+		// 		voxels.setLayerLookupTable(idx, numTileLayers);
+		// 		numTileLayers++;
+		// 	}
+		// }
+
+		const voxels: Map<string, VoxelCell>[] = [];
+
+		let layerIdx = 0;
+		let voxelData = generateVoxelsFromLayerData(floorLayer, 0);
+		let prunedVoxels = pruneCells(voxelData, voxels[layerIdx]);
+		let voxelVerts = buildMeshDataFromCells(prunedVoxels);
+		let layerColliderDesc = RAPIER.ColliderDesc.trimesh(
+			Float32Array.from(voxelVerts.positions),
+			Uint32Array.from(voxelVerts.indices)
+		);
+		world.createCollider(layerColliderDesc);
+
+		layerIdx = 2;
+		voxelData = generateVoxelsFromLayerData(wallsLayer, 0);
+		prunedVoxels = pruneCells(voxelData, voxels[layerIdx]);
+		voxelVerts = buildMeshDataFromCells(prunedVoxels);
+		layerColliderDesc = RAPIER.ColliderDesc.trimesh(
+			Float32Array.from(voxelVerts.positions),
+			Uint32Array.from(voxelVerts.indices)
+		);
+		layerColliderDesc.setTranslation(0, layerIdx, 0);
+		world.createCollider(layerColliderDesc);
+
+		layerIdx = 3;
+		voxelData = generateVoxelsFromLayerData(treesLayer, 0);
+		prunedVoxels = pruneCells(voxelData, voxels[layerIdx]);
+		voxelVerts = buildMeshDataFromCells(prunedVoxels);
+		layerColliderDesc = RAPIER.ColliderDesc.trimesh(
+			Float32Array.from(voxelVerts.positions),
+			Uint32Array.from(voxelVerts.indices)
+		);
+		layerColliderDesc.setTranslation(0, layerIdx, 0);
+		world.createCollider(layerColliderDesc);
 	}
 
 	update(dt: number): void {
@@ -263,4 +327,228 @@ type PhysicsOptions = {
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 	module.exports = PhysicsComponent;
+}
+
+type LayerData = {
+	id: number;
+	name: string;
+	type: 'tilelayer' | 'objectgroup';
+	width: number;
+	height: number;
+	data: number[];
+};
+
+type VoxelCell = {
+	position: number[];
+	type: number;
+	visible: boolean;
+	hiddenFaces: boolean[];
+	isPreview: boolean;
+};
+
+function getKeyFromPos(x: number, y: number, z: number) {
+	return `${x}.${y}.${z}`;
+}
+
+function findNeighbors(x: number, y: number, z: number) {
+	const k1 = getKeyFromPos(x + 1, y, z);
+	const k2 = getKeyFromPos(x - 1, y, z);
+	const k3 = getKeyFromPos(x, y + 1, z);
+	const k4 = getKeyFromPos(x, y - 1, z);
+	const k5 = getKeyFromPos(x, y, z + 1);
+	const k6 = getKeyFromPos(x, y, z - 1);
+
+	const neighborKeys = [k1, k2, k3, k4, k5, k6];
+	return neighborKeys;
+}
+
+function updateCellSides(curCell: VoxelCell, cells: Map<string, VoxelCell>) {
+	let visible = false;
+	const neighborKeys = findNeighbors(curCell.position[0], curCell.position[1], curCell.position[2]);
+	for (let i = 0; i < 6; ++i) {
+		const hasNeighbor = cells.has(neighborKeys[i]);
+
+		curCell.hiddenFaces[i] = hasNeighbor;
+
+		if (!hasNeighbor) {
+			visible = true;
+		}
+	}
+	return visible;
+}
+
+function generateVoxelsFromLayerData(data: LayerData, height: number, flat = false) {
+	const voxels = new Map<string, VoxelCell>();
+	const allFacesVisible = [false, false, false, false, false, false];
+	const onlyBottomFaceVisible = [true, true, true, false, true, true];
+	const hiddenFaces = flat ? onlyBottomFaceVisible : allFacesVisible;
+
+	const yOffset = 0.001;
+
+	for (let z = 0; z < data.height; z++) {
+		for (let x = 0; x < data.width; x++) {
+			let tileId = data.data[z * data.width + x];
+			if (tileId <= 0) continue;
+			tileId -= 1;
+
+			const pos = { x: x + 0.5, y: height + yOffset * height, z: z + 0.5 };
+
+			voxels.set(getKeyFromPos(pos.x, pos.y, pos.z), {
+				position: [pos.x, pos.y, pos.z],
+				type: tileId,
+				visible: true,
+				hiddenFaces: [...hiddenFaces],
+				isPreview: false,
+			});
+		}
+	}
+	return voxels;
+}
+
+function pruneCells(cells: Map<string, VoxelCell>, prevCells?: Map<string, VoxelCell>) {
+	const prunedVoxels = prevCells ?? new Map<string, VoxelCell>();
+	for (let k of cells.keys()) {
+		const curCell = cells.get(k);
+
+		if (prevCells && curCell.type < 0) {
+			let pos = curCell.position;
+			prevCells.delete(k);
+			findNeighbors(pos[0], pos[1], pos[2]).forEach((neighborKey) => {
+				let neighbor = prevCells.get(neighborKey);
+				if (neighbor !== undefined) {
+					updateCellSides(neighbor, prevCells);
+				}
+			});
+		} else {
+			let visible = updateCellSides(curCell, cells) && (prevCells === undefined || updateCellSides(curCell, prevCells));
+			if (visible) {
+				prunedVoxels.set(k, curCell);
+			}
+		}
+	}
+	return prunedVoxels;
+}
+
+function buildMeshDataFromCells(cells: Map<string, VoxelCell>) {
+	// const xStep = tileset.tileWidth / tileset.width;
+	// const yStep = tileset.tileHeight / tileset.height;
+
+	const pxGeometry = new THREE.PlaneGeometry(1, 1);
+	pxGeometry.rotateY(Math.PI / 2);
+	pxGeometry.translate(0.5, 0, 0);
+
+	const nxGeometry = new THREE.PlaneGeometry(1, 1);
+	nxGeometry.rotateY(-Math.PI / 2);
+	nxGeometry.translate(-0.5, 0, 0);
+
+	const pyGeometry = new THREE.PlaneGeometry(1, 1);
+	pyGeometry.rotateX(-Math.PI / 2);
+	pyGeometry.translate(0, 0.5, 0);
+
+	const nyGeometry = new THREE.PlaneGeometry(1, 1);
+	nyGeometry.rotateX(Math.PI / 2);
+	nyGeometry.translate(0, -0.5, 0);
+
+	const pzGeometry = new THREE.PlaneGeometry(1, 1);
+	pzGeometry.translate(0, 0, 0.5);
+
+	const nzGeometry = new THREE.PlaneGeometry(1, 1);
+	nzGeometry.rotateY(Math.PI);
+	nzGeometry.translate(0, 0, -0.5);
+
+	const invertUvs = [nyGeometry];
+
+	const geometries = [pxGeometry, nxGeometry, pyGeometry, nyGeometry, pzGeometry, nzGeometry];
+
+	const meshes = {
+		positions: [],
+		uvs: [],
+		normals: [],
+		indices: [],
+		topIndices: [],
+		sidesIndices: [],
+		previewTopIndices: [],
+		previewSidesIndices: [],
+	};
+
+	for (let c of cells.keys()) {
+		const curCell = cells.get(c);
+
+		for (let i = 0; i < geometries.length; ++i) {
+			if (curCell.hiddenFaces[i]) {
+				continue;
+			}
+
+			const targetData = meshes;
+
+			const bi = targetData.positions.length / 3;
+			const localPositions = [...geometries[i].attributes.position.array];
+			for (let j = 0; j < 3; ++j) {
+				for (let v = 0; v < 4; ++v) {
+					localPositions[v * 3 + j] += curCell.position[j];
+				}
+			}
+			targetData.positions.push(...localPositions);
+
+			// const xIdx = curCell.type % tileset.cols;
+			// const yIdx = Math.floor(curCell.type / tileset.cols);
+
+			// const singlePixelOffset = { x: xStep / tileset.tileWidth, y: yStep / tileset.tileHeight };
+			// const halfPixelOffset = { x: singlePixelOffset.x / 2, y: singlePixelOffset.y / 2 };
+
+			// const xOffset = xStep * xIdx + halfPixelOffset.x;
+			// const yOffset = 1 - yStep * yIdx - yStep - halfPixelOffset.y;
+
+			// if (invertUvs.includes(geometries[i])) {
+			// 	geometries[i].attributes.uv.array[4] = xOffset;
+			// 	geometries[i].attributes.uv.array[5] = yOffset + yStep;
+
+			// 	geometries[i].attributes.uv.array[6] = xOffset + xStep - singlePixelOffset.x;
+			// 	geometries[i].attributes.uv.array[7] = yOffset + yStep;
+
+			// 	geometries[i].attributes.uv.array[0] = xOffset;
+			// 	geometries[i].attributes.uv.array[1] = yOffset + singlePixelOffset.y;
+
+			// 	geometries[i].attributes.uv.array[2] = xOffset + xStep - singlePixelOffset.x;
+			// 	geometries[i].attributes.uv.array[3] = yOffset + singlePixelOffset.y;
+			// } else {
+			// 	geometries[i].attributes.uv.array[0] = xOffset;
+			// 	geometries[i].attributes.uv.array[1] = yOffset + yStep;
+
+			// 	geometries[i].attributes.uv.array[2] = xOffset + xStep - singlePixelOffset.x;
+			// 	geometries[i].attributes.uv.array[3] = yOffset + yStep;
+
+			// 	geometries[i].attributes.uv.array[4] = xOffset;
+			// 	geometries[i].attributes.uv.array[5] = yOffset + singlePixelOffset.y;
+
+			// 	geometries[i].attributes.uv.array[6] = xOffset + xStep - singlePixelOffset.x;
+			// 	geometries[i].attributes.uv.array[7] = yOffset + singlePixelOffset.y;
+			// }
+
+			targetData.uvs.push(...geometries[i].attributes.uv.array);
+			targetData.normals.push(...geometries[i].attributes.normal.array);
+
+			const localIndices = [...geometries[i].index.array];
+			for (let j = 0; j < localIndices.length; ++j) {
+				localIndices[j] += bi;
+			}
+
+			// top and bottom face
+			if (i === 2 || i === 3) {
+				if (curCell.isPreview) {
+					targetData.previewTopIndices.push(...localIndices);
+				} else {
+					targetData.indices.push(...localIndices);
+				}
+			} else {
+				if (curCell.isPreview) {
+					targetData.previewSidesIndices.push(...localIndices);
+				} else {
+					targetData.indices.push(...localIndices);
+				}
+			}
+		}
+	}
+
+	return meshes;
 }
